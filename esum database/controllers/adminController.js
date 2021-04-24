be1d14b935e038ab22c67d53dd119149ee770bd2
Brady Madden <81207895+Brady-Madden@users.noreply.github.com>
2021-04-24 02:22:55 -0400

    Update adminController.js

---
+++ esum database/controllers/adminController.js
@@ -0,0 +1,43 @@
+const monAdmin = require('../models/admin');
+
+const admin_create = (req,res) => {
+    const adminEmail = req.body.adminEmail;
+    const password = req.body.password;
+
+    const newAdmin = new monAdmin({adminEmail:adminEmail, password:password});
+    newAdmin.save()
+        .then(result => {
+            res.send(result);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+}
+
+const admin_index = (req,res) => {
+    monAdmin.find().sort({createdAt: -1})
+        .then(result => {
+            res.send(result);
+        })
+        .catch(error => {
+            res.send(error);
+        })
+}
+
+const admin_delete = (req,res) => {
+    const id = req.params.id;
+
+    monAdmin.findByIdAndDelete(id)
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+module.exports = {
+    admin_create,
+    admin_index,
+    admin_delete
+}
