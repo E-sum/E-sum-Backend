411f5a1f58b664c9a011c4b701cf897591afe15b
brady madden <bradymadden16@gmail.com>
2021-04-24 02:11:19 -0400

    -new dashboard -fixed routing issues -logout feature -token based authentication linked to user

--- esum database/routes/emailRoutes.js
+++ esum database/routes/emailRoutes.js
@@ -4,7 +4,7 @@ const emailController = require('../controllers/emailController');
 const router = express.Router();
 
 router.get('/create', emailController.email_create_get);
-router.get('/', emailController.email_index);
+router.get('/index', emailController.email_index);
 router.post('/', emailController.email_create_post);
 router.get('/:id', emailController.email_details);
 router.delete('/:id', emailController.email_delete);
